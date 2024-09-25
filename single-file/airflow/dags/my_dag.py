417c8d12b0405f4f9f5f4a89a63fc6ae4ed9cee6
Michael DiNunzio <mdinunzio@gmail.com>
2024-09-24 23:15:23 -0400

    Up to 10:53 in video

--- single-file/airflow/dags/my_dag.py
+++ single-file/airflow/dags/my_dag.py
@@ -7,26 +7,9 @@ from datetime import datetime
 from random import randint
 
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import BranchPythonOperator, PythonOperator
-
-
-def _choose_best_model(ti):
-    accuracies = ti.xcom_pull(
-        task_ids=[
-            "training_model_A",
-            "training_model_B",
-            "training_model_C",
-        ]
-    )
-    best_accuracy = max(accuracies)
-    if best_accuracy > 8:
-        return "accurate"
-    return "inaccurate"
-
-
-def _training_model():
-    return randint(1, 10)
 
 
 with DAG(
@@ -36,28 +19,20 @@ with DAG(
     description="Training ML Models.",
     tags=["data_engineering", "Mark"],
     catchup=False,
-) as dag:
-    training_model_A = PythonOperator(
-        task_id="training_model_A", python_callable=_training_model
-    )
+):
 
-    training_model_B = PythonOperator(
-        task_id="training_model_B", python_callable=_training_model
-    )
+    @task
+    def training_model():
+        return randint(1, 10)
 
-    training_model_C = PythonOperator(
-        task_id="training_model_C", python_callable=_training_model
-    )
-
-    choose_best_model = BranchPythonOperator(
-        task_id="choose_best_model", python_callable=_choose_best_model
-    )
+    @task.branch
+    def choose_best_model(accuracies):
+        best_accuracy = max(accuracies)
+        if best_accuracy > 8:
+            return "accurate"
+        return "inaccurate"
 
     accurate = BashOperator(task_id="accurate", bash_command="echo 'accurate'")
     inaccurate = BashOperator(task_id="inaccurate", bash_command="echo 'inaccurate'")
 
-    (
-        [training_model_A, training_model_B, training_model_C]
-        >> choose_best_model
-        >> [accurate, inaccurate]
-    )
+    choose_best_model(training_model())
