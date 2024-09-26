d4da0f6d6fcc10a773bd7e0b22b727947ca89054
Michael DiNunzio <mdinunzio@gmail.com>
2024-09-25 21:09:27 -0400

    Complete dag sensors video

---
+++ single-file/airflow/dags/dag_sensor.py
@@ -0,0 +1,19 @@
+# https://youtu.be/fgm3BZ3Ubnw?si=rLs7W0J1W2MAm30a
+from airflow.models import DAG
+from airflow.sensors.filesystem import FileSensor
+
+from datetime import datetime
+
+with DAG(
+    "dag_sensor",
+    schedule_interval="@daily",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+) as dag:
+    waiting_for_file = FileSensor(
+        task_id="waiting_for_file",
+        poke_interval=30,
+        timeout=60 * 5,
+        mode="reschedule",
+        soft_fail=True,
+    )
