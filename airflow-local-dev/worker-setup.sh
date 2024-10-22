6de539c00b8978bd2f232f167e59a794c3d47810
Michael DiNunzio <mdinunzio@gmail.com>
2024-10-22 00:40:59 -0400

    Add worker setup

---
+++ airflow-local-dev/worker-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Create a virtual environments
+mkdir /opt/airflow/envs
+python -m venv /opt/airflow/envs/finances
+# /opt/airflow/envs/finances/bin/python -m pip install -r /opt/airflow/requirements.txt
+
+# Start the celery worker
+celery worker
