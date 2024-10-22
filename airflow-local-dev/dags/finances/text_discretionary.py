93b7fa15aa86b365b9d4abb78c8f37b1551755f5
Michael DiNunzio <mdinunzio@gmail.com>
2024-10-22 00:41:12 -0400

    Update texter

--- airflow-local-dev/dags/finances/text_discretionary.py
+++ airflow-local-dev/dags/finances/text_discretionary.py
@@ -11,16 +11,18 @@ sys.path.append(r"/opt/airflow/code/finances")
 import finances.io.ynab
 from finances.job.text_discretionary import RECIPIENT, TODAY
 
-america_new_york = pendulum.timezone("America/New_York")
+AMERICA_NEW_YORK = pendulum.timezone("America/New_York")
+AIRFLOW_EMAIL_ALERT_LIST = os.environ["AIRFLOW_EMAIL_ALERT_LIST"]
+AIRFLOW_PYTHON_EXECUTABLE = os.environ["AIRFLOW_PYTHON_EXECUTABLE"]
 
 
 @dag(
     schedule="30 10 * * *",
-    start_date=datetime.datetime(2024, 10, 15, 10, 30, 0, 0, tzinfo=america_new_york),
+    start_date=datetime.datetime(2024, 10, 15, 10, 30, 0, 0, tzinfo=AMERICA_NEW_YORK),
     catchup=False,
     tags=["ynab"],
     default_args={
-        "email": os.environ["AIRFLOW_EMAIL_ALERT_LIST"],
+        "email": AIRFLOW_EMAIL_ALERT_LIST,
         "email_on_failture": True,
         "email_on_retry": False,
         "retries": 2,
@@ -29,7 +31,7 @@ america_new_york = pendulum.timezone("America/New_York")
 )
 def text_discretionary():
 
-    @task
+    @task.external_python(python=AIRFLOW_PYTHON_EXECUTABLE)
     def fetch_single_date_budget_frame():
         budget = finances.io.ynab.fetch_single_date_budget_frame(
             TODAY.year, TODAY.month
